73f9c619a2341a150756e051560d4b71913cc1f7
Muhammad Umar Abid <mumarabid656@gmail.com>
2025-05-18 18:03:59 +0200

    import name change 7

--- src/pages/shop/ShopAllRefunds.jsx
+++ src/pages/shop/ShopAllRefunds.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
-import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar";
-import AllRefundOrders from "../../components/Shop/AllRefundOrders";
+import DashboardHeader from "../../components/shop/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/shop/Layout/DashboardSideBar";
+import AllRefundOrders from "../../components/shop/AllRefundOrders";
 
 const ShopAllRefunds = () => {
   return (
