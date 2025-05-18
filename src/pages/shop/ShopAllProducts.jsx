421d959377d8ae700b072d5572cbed054bbeebf9
Muhammad Umar Abid <mumarabid656@gmail.com>
2025-05-18 18:05:19 +0200

    import name change 8

--- src/pages/shop/ShopAllProducts.jsx
+++ src/pages/shop/ShopAllProducts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
-import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar";
-import AllProducts from "../../components/Shop/AllProducts";
+import DashboardHeader from "../../components/shop/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/shop/Layout/DashboardSideBar";
+import AllProducts from "../../components/shop/AllProducts";
 
 const ShopAllProducts = () => {
   return (
